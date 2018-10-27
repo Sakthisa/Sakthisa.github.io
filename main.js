351d929b8ae99af793f40b4e4fc6995b0e67ca04
Sakthisa <sakthisa@oregonstate.edu>
2018-10-27 14:10:29 -0700

    Fixed

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
-var ourRequest = new XMLHttpRequest();
+var count = 1;
+var animalContainer = document.getElementById("animal-info");
+var button = document.getElementById("btn");
 
-//Get JSON information from website
-ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+button.addEventListener("click", function() {
+  var ourRequest = new XMLHttpRequest();
 
-//What will happen on the website when the information is received
-ourRequest.onload = function(){
-  var ourData = JSON.parse(ourRequest.responseText); //Make the text-file an actual json file, so we can work with objects
-  console.log(ourData[0]);
-};
+  //Get JSON information from website
+  ourRequest.open('GET', 'TA00011.json', true);
 
-//Sending the request
-ourRequest.send();
+  //What will happen on the website when the information is received
+  ourRequest.onload = function() {
+    var ourData = JSON.parse(ourRequest.responseText); //Make the text-file an actual json file, so we can work with objects
+    console.log(ourData);
+    //renderHTML(ourData);
+  };
+
+  //Sending the request
+  ourRequest.send();
+  count++;
+  if(count > 3){
+    button.classList.add("hide-me");
+  }
+});
+
+function renderHTML(ourData) {
+  var htmlString = "";
+
+  for(i = 0; i < ourData.length; i++){
+    htmlString += "<p>" + ourData[i].name + " is a " + ourData[i].species + "." + " That likes to eat ";
+    for(j = 0; j < ourData[i].foods.likes.length; j++){
+      htmlString += " " + ourData[i].foods.likes[j];
+    }
+    htmlString  += ".</p>"
+  }
+
+
+  animalContainer.insertAdjacentHTML('beforeend', htmlString);
+}
